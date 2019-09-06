01b789293d8bab2ddb68773df0e266fdd981cd25
Administrator <Administrator@PC-20190530KLTS>
2019-09-06 13:41:05 +0800

    新增原型模式

---
+++ src/com/wzx/prototype/Sheep.java
@@ -0,0 +1,45 @@
+package com.wzx.prototype;
+
+import java.util.Date;
+
+public class Sheep implements Cloneable {
+	String name;
+	Date birthday;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public Sheep(String name, Date birthday) {		
+		this.name = name;
+		this.birthday = birthday;
+	}
+	public Sheep() {		
+		
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		
+		Object obj=super.clone();		
+		
+		return obj;
+	}
+
+	protected Object clone1() throws CloneNotSupportedException {
+		
+		Object obj=super.clone();
+		Sheep s=(Sheep)obj;
+		s.birthday=(Date)this.birthday.clone();
+		
+		return obj;
+	}
+	
+}
